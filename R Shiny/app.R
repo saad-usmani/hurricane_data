3bdcc6a2d9a372273b96d6ec2b8d7422851f6ac9
saad-usmani <31375773+saad-usmani@users.noreply.github.com>
2017-11-28 20:57:37 -0500

    added new function and updated UI

--- R Shiny/app.R
+++ R Shiny/app.R
@@ -13,14 +13,23 @@ ui<-fluidPage(
                       sidebarPanel(
                         tags$div(
                           tags$p("Choose a system by its year and name and discover all other similar hurricanes with similar origins.")),
+                        tags$head(tags$style("#origin{height:100vh !important;}")),
                         selectInput(inputId = "year", label = "Year", as.list(years)),
                         textInput(inputId = "name", label = "Name of Storm", "Name"),
-                        actionButton("goButton", "Go!")
+                        actionButton("goButton", "Go!"), width = 2
                       ),
                       mainPanel(
-                        plotlyOutput("origin")
+                        plotlyOutput("origin"), width = 10
                       )
                       
+             ),
+             tabPanel("Plot2",
+                     sidebarPanel(
+                               tags$div(
+                                 tags$p("Finding top 5 closest tracks.")),
+                               tags$head(tags$style("#tracks{height:100vh !important;}")),
+                               tableOutput("table"), width = 2),
+                     mainPanel(plotlyOutput("tracks"), width = 10)
              )
   )
 )
@@ -41,13 +50,50 @@ server<- function(input, output){
       layout(
         plot_bgcolor = '#191A1A', paper_bgcolor = '#191A1A',
         mapbox = list(style = 'dark',
-                      zoom = 1.5,
+                      zoom = 3,
                       center = list(lat = median(hur$Latitude),
                                     lon = median(hur$Longitude))),
         margin = list(l = 0, r = 0,
                       b = 0, t = 0,
                       pad = 0),
         showlegend=FALSE)
+  })
+  output$tracks<-renderPlotly({
+    input$goButton
+    data1<-reactive({hur$Date >= as.Date(paste(isolate(input$year),"-01-01", sep='')) & hur$Date <= as.Date(paste(isolate(input$year),"-12-31",sep='')) & hur$Name == toupper(isolate(input$name))})
+    example2<-filter(hur, data1())
+    ex<-filter(hur, Latitude >= (example2$Latitude-1) &
+               Latitude<=example2$Latitude+1 &
+               Longitude >= (example2$Longitude-1) &
+               Longitude<=example2$Longitude+1)
+    ex<-top_n(arrange(count(ex,ID), desc(n)), 5)
+    ex_join<-semi_join(hur,ex)
+    p7 <- plot_mapbox(mode = 'scatterbox') %>%
+      add_markers(
+        data = ex_join, x = ~Longitude, y = ~Latitude, text=~paste('Name: ', Name, '<br>Max Wind:', Maximum.Wind, '<br>Date: ', Date), color=~ID,
+        size = ~Maximum.Wind, alpha = 0.5) %>%
+      layout(
+        plot_bgcolor = '#191A1A', paper_bgcolor = '#191A1A',
+        mapbox = list(style = 'dark',
+                      zoom = 3,
+                      center = list(lat = median(hur$Latitude),
+                                    lon = median(hur$Longitude))),
+        margin = list(l = 0, r = 0,
+                      b = 0, t = 0,
+                      pad = 0),
+        showlegend=FALSE)
+    p7
+  })
+  output$table<-renderTable({
+    data1<-reactive({hur$Date >= as.Date(paste(isolate(input$year),"-01-01", sep='')) & hur$Date <= as.Date(paste(isolate(input$year),"-12-31",sep='')) & hur$Name == toupper(isolate(input$name))})
+    example2<-filter(hur, data1())
+    input$goButton
+    ex<-filter(hur, Latitude >= (example2$Latitude-1) &
+                 Latitude<=example2$Latitude+1 &
+                 Longitude >= (example2$Longitude-1) &
+                 Longitude<=example2$Longitude+1)
+    counter<-data.frame(arrange(count(group_by(ex,ID,Name)), desc(n)))
+    top_n(counter, 5, n)
   })
 }
 
