4231d296f906917363c804fb2aa7155f75951e95
saad-usmani <saadu.usmani@gmail.com>
2017-12-12 13:05:12 -0500

    Commented out the heatmap stuff for now in the shiny App because it was giving me issues about running the Google API too many times. The tab also looked ugly. I also implemented a working version of predicting tracks based on given lat/lng and windspeed. Success!

--- R Shiny/app.R
+++ R Shiny/app.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(lubridate)
 library(ggplot2)
 library(data.table)
+library(ggmap)
 
 ###Data Import and Simple Cleaning
 hur <- fread(file.path("C:/Users/susmani/Documents/Year 5/Courses/Data Munging/GroupProject2/hurricane.csv"),na.strings = c("PrivacySuppressed", "NULL"))
@@ -14,7 +15,7 @@ hur$Latitude<-as.numeric(unlist(strsplit(hur$Latitude, split='N', fixed=TRUE)))
 hur$Longitude<-as.numeric(unlist(strsplit(hur$Longitude, split='W', fixed=TRUE)))
 hur$Longitude<-(-1)*hur$Longitude
 hur_start<-hur[!duplicated(hur$ID), ] #Dataset of only origin of systems.
-atlantic_map<-get_map(location = "puerto rico", zoom = 4)
+#atlantic_map<-get_map(location = "puerto rico", zoom = 4)
 ###Actual App Below
 
 years<-c(1851:2015)
@@ -44,17 +45,17 @@ ui<-fluidPage(
                       )
                       
              ),
-             tabPanel("Heat Maps",
-                      sidebarPanel(
-                        tags$div(
-                          tags$p("Creates a heatmap.")),
-                        tags$head(tags$style("#origin{height:100vh !important;}")), width = 2
-                      ), 
-                      tabsetPanel(
-                        tabPanel(plotOutput("heat"), width = 10),
-                        tabPanel(plotOutput("hj"))
-                      )
-             ),
+             # tabPanel("Heat Maps",
+             #          sidebarPanel(
+             #            tags$div(
+             #              tags$p("Creates a heatmap.")),
+             #            tags$head(tags$style("#origin{height:100vh !important;}")), width = 2
+             #          ), 
+             #          tabsetPanel(
+             #            tabPanel(plotOutput("heat"), width = 10),
+             #            tabPanel(plotOutput("hj"))
+             #          )
+             # ),
              tabPanel("Closest Paths",
                       sidebarLayout(
                         sidebarPanel(
@@ -63,20 +64,24 @@ ui<-fluidPage(
                           tags$head(tags$style("#tracks{height:100vh !important;}")),
                           tableOutput("table"), width = 2),
                         mainPanel(plotlyOutput("tracks"), width = 10))
-             ),
+              ),
              tabPanel("Path Finder",
                       sidebarPanel(
                         tags$div(
-                          tags$p("Creates a heatmap.")),
-                        tags$head(tags$style("#origin{height:100vh !important;}")), width = 2
-                      ),
-                      mainPanel(
-                        plotlyOutput("predict"), width = 10
+                          tags$p("Choose a system by its year and name and discover all other similar hurricanes with similar origins.")),
+                        tags$head(tags$style("#predict{height:100vh !important;}")),
+                          textInput(inputId = "lat", label = "Enter Latitude"),
+                          textInput(inputId = "lng", label = "Enter Longitude"),
+                          textInput(inputId = "wind", label = "Enter wind"),
+                          actionButton("goButton2", "Go!"),
+                          width = 2
+                        ),
+                        mainPanel(
+                          plotlyOutput("predict"), width = 10
+                          )
                       )
              )
-             
   )
-)
 
 
 server<- function(input, output){
@@ -106,20 +111,20 @@ server<- function(input, output){
                       pad = 0),
         showlegend=FALSE)
   })
-  output$heat<-renderPlot({
-    input$goButton
-    heat1<-reactive({hur[hur$Date >= as.Date(paste(isolate(input$year),"-01-01", sep='')) & hur$Date <= as.Date(paste(isolate(input$year),"-12-31",sep='')) & hur$Name == toupper(isolate(input$names)),]})
-    heat2<-reactive({top_n(select(heat1(), Latitude, Longitude),1)})
-    heatf<-reactive({hur_start[hur_start$Latitude >= heat2()$Latitude-1 & hur_start$Latitude<=heat2()$Latitude+1 & hur_start$Longitude >= heat2()$Longitude-1 & hur_start$Longitude<=heat2()$Longitude+1,]})
-    heat3<-reactive({top_n(heatf(), 10)})
-    fullheat<-reactive({semi_join(hur, heat3(), by = "ID")})
-    heat<-ggmap(atlantic_map, extent = "device") + geom_density2d(data = fullheat(), 
-                                                                  aes(x = Longitude, y = Latitude), size = 0.3) + stat_density2d(data = fullheat(), 
-                                                                                                                                 aes(x = Longitude, y = Latitude, fill = ..level.., alpha = ..level..), size = 0.01, 
-                                                                                                                                 bins = 16, geom = "polygon") + scale_fill_gradient(low = "green", high = "red") + 
-      scale_alpha(range = c(0, 0.3), guide = FALSE)
-    heat
-  }, width = 800, height = 600)
+  # output$heat<-renderPlot({
+  #   input$goButton
+  #   heat1<-reactive({hur[hur$Date >= as.Date(paste(isolate(input$year),"-01-01", sep='')) & hur$Date <= as.Date(paste(isolate(input$year),"-12-31",sep='')) & hur$Name == toupper(isolate(input$names)),]})
+  #   heat2<-reactive({top_n(select(heat1(), Latitude, Longitude),1)})
+  #   heatf<-reactive({hur_start[hur_start$Latitude >= heat2()$Latitude-1 & hur_start$Latitude<=heat2()$Latitude+1 & hur_start$Longitude >= heat2()$Longitude-1 & hur_start$Longitude<=heat2()$Longitude+1,]})
+  #   heat3<-reactive({top_n(heatf(), 10)})
+  #   fullheat<-reactive({semi_join(hur, heat3(), by = "ID")})
+  #   heat<-ggmap(atlantic_map, extent = "device") + geom_density2d(data = fullheat(), 
+  #                                                                 aes(x = Longitude, y = Latitude), size = 0.3) + stat_density2d(data = fullheat(), 
+  #                                                                                                                                aes(x = Longitude, y = Latitude, fill = ..level.., alpha = ..level..), size = 0.01, 
+  #                                                                                                                                bins = 16, geom = "polygon") + scale_fill_gradient(low = "green", high = "red") + 
+  #     scale_alpha(range = c(0, 0.3), guide = FALSE)
+  #   heat
+  # }, width = 800, height = 600)
   output$tracks<-renderPlotly({
     input$goButton
     data1<-reactive({hur$Date >= as.Date(paste(isolate(input$year),"-01-01", sep='')) & hur$Date <= as.Date(paste(isolate(input$year),"-12-31",sep='')) & hur$Name == toupper(isolate(input$names))})
@@ -156,6 +161,27 @@ server<- function(input, output){
                  Longitude<=example2$Longitude+1)
     counter<-data.frame(arrange(count(group_by(ex,ID,Name)), desc(n)))
     top_n(counter, 5, n)
+  })
+  output$predict<-renderPlotly({
+    input$goButton2
+    full_input<-reactive({hur[hur$Latitude >= (as.numeric(isolate(input$lat))-1) & hur$Latitude <= (as.numeric(isolate(input$lat))+1) & hur$Longitude >= (as.numeric(isolate(input$lng))-1) & hur$Longitude <= (as.numeric(isolate(input$lng))+1) & hur$Maximum.Wind>=(as.numeric(isolate(input$wind))-5) & hur$Maximum.Wind <= (as.numeric(isolate(input$wind))+5),]})
+    full_join<-reactive({arrange(count(group_by(full_input(), ID), Maximum.Wind), desc(Maximum.Wind))[1:5,]})
+    full_join2<-reactive({semi_join(hur, full_join(), by = "ID")})
+    path.plot <- plot_mapbox(mode = 'scatterbox') %>%
+      add_markers(
+        data = full_join2(), x = ~Longitude, y = ~Latitude, text=~paste('Name: ', Name, '<br>Max Wind:', Maximum.Wind, '<br>Date: ', Date), color=~ID,
+        size = ~Maximum.Wind, alpha = 0.5) %>%
+      layout(
+        plot_bgcolor = '#191A1A', paper_bgcolor = '#191A1A',
+        mapbox = list(style = 'dark',
+                      zoom = 3.5,
+                      center = list(lat = median(hur$Latitude),
+                                    lon = median(hur$Longitude))),
+        margin = list(l = 0, r = 0,
+                      b = 0, t = 0,
+                      pad = 0),
+        showlegend=FALSE)
+    path.plot
   })
 }
 
